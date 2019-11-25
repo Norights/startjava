22cb12e32c7ddd74d11c6a60496d8217527f036d
Norights <norights2@gmail.com>
2019-11-25 22:25:17 +0300

    Homework 1_1v2

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,21 +1,21 @@
     public class Variable{
 
         public static void main(String[] args) {
-            byte HDD = 2;
-                System.out.println("HDD: " + HDD + " pieces");
+            byte hdd = 2;
+            System.out.println("hdd: " + hdd + " pieces");
             short computerRAM = 16000;
-                System.out.println("computer RAM: " + computerRAM + " mb");
-            int howoldisit = 5;
-                System.out.println("how old is it: " + howoldisit + " years");
+            System.out.println("computer RAM: " + computerRAM + " mb");
+            int howOldIsIt = 5;
+            System.out.println("how old is it: " + howOldIsIt + " years");
             long totalHoursOfWork = 43805; //If it worked 5 years without stopping=)
-                System.out.println("Total hours of work: " + totalHoursOfWork + " hours");
+            System.out.println("Total hours of work: " + totalHoursOfWork + " hours");
             float freeDiskSpace = 54.4f;
-                System.out.println("Free disk space (C): " + freeDiskSpace + " Gb");
+            System.out.println("Free disk space (C): " + freeDiskSpace + " Gb");
             double cpuFrequency = 3.4;
-                System.out.println("cpu Frequency: " + cpuFrequency + " MHz");
-            char nameofdiscwithphotos = 'I';
-                System.out.println("Name of disc with photos: " + nameofdiscwithphotos);
-            boolean doIlikeJava = true;
-                System.out.println("Do I like Java? - " + doIlikeJava);        
+            System.out.println("cpu Frequency: " + cpuFrequency + " MHz");
+            char nameOfDiscWithPhotos = 'I';
+            System.out.println("Name of disc with photos: " + nameOfDiscWithPhotos);
+            boolean doILikeJava = true;
+            System.out.println("Do I like Java? - " + doILikeJava);        
         }
     }
