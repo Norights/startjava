6fc31c597161c0f44f41694553b09b3c2e4f1938
Norights <norights2@gmail.com>
2019-11-25 21:01:08 +0300

    Homework 1_1

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,20 +1,21 @@
     public class Variable{
+
         public static void main(String[] args) {
-            byte  HDD = 2;
+            byte HDD = 2;
                 System.out.println("HDD: " + HDD + " pieces");
             short computerRAM = 16000;
                 System.out.println("computer RAM: " + computerRAM + " mb");
             int howoldisit = 5;
                 System.out.println("how old is it: " + howoldisit + " years");
-            long totalhoursofwork = 43805; //If it worked 5 years without stopping=)
-                System.out.println("Total hours of work: " + totalhoursofwork + " hours");
-            float freediskspace = 54.4f;
-                System.out.println("Free disk space (C): " + freediskspace + " Gb");
-            double CPUfrequency = 3.4;
-                System.out.println("CPU frequency: " + CPUfrequency + " MHz");
-            char nameofthediscwithphotos = 'I';
-                System.out.println("Name of the disc with photos: " + nameofthediscwithphotos);
-            boolean DoIlikeJava = true;
-                System.out.println("Do I like Java? - " + DoIlikeJava);        
+            long totalHoursOfWork = 43805; //If it worked 5 years without stopping=)
+                System.out.println("Total hours of work: " + totalHoursOfWork + " hours");
+            float freeDiskSpace = 54.4f;
+                System.out.println("Free disk space (C): " + freeDiskSpace + " Gb");
+            double cpuFrequency = 3.4;
+                System.out.println("cpu Frequency: " + cpuFrequency + " MHz");
+            char nameofdiscwithphotos = 'I';
+                System.out.println("Name of disc with photos: " + nameofdiscwithphotos);
+            boolean doIlikeJava = true;
+                System.out.println("Do I like Java? - " + doIlikeJava);        
         }
     }
