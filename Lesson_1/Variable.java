70d5a17eb765979c0da9e2b4082e35f51e43a14b
Norights <norights2@gmail.com>
2019-11-26 16:21:34 +0300

    Homework 1 z1 v3

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,12 +1,12 @@
-    public class Variable{
+    public class Variable {
 
         public static void main(String[] args) {
             byte hdd = 2;
             System.out.println("hdd: " + hdd + " pieces");
-            short computerRAM = 16000;
-            System.out.println("computer RAM: " + computerRAM + " mb");
-            int howOldIsIt = 5;
-            System.out.println("how old is it: " + howOldIsIt + " years");
+            short computerRam = 16000;
+            System.out.println("computer Ram: " + computerRam + " mb");
+            int lifeTime = 5;
+            System.out.println("Lifetime: " + lifeTime + " years");
             long totalHoursOfWork = 43805; //If it worked 5 years without stopping=)
             System.out.println("Total hours of work: " + totalHoursOfWork + " hours");
             float freeDiskSpace = 54.4f;
@@ -15,7 +15,7 @@
             System.out.println("cpu Frequency: " + cpuFrequency + " MHz");
             char nameOfDiscWithPhotos = 'I';
             System.out.println("Name of disc with photos: " + nameOfDiscWithPhotos);
-            boolean doILikeJava = true;
-            System.out.println("Do I like Java? - " + doILikeJava);        
+            boolean isLikeJava = true;
+            System.out.println("Is like Java? - " + isLikeJava);        
         }
     }
