de1483deb53905a941d1fca24681bafc61609187
Norights <norights2@gmail.com>
2019-11-27 15:51:23 +0300

    Homework 1 z1 z2 v3

---
+++ Lesson_1/Cycle.java
@@ -0,0 +1,21 @@
+    public class Cycle {
+        public static void main(String[] args) {
+            for(int i = 0; i <= 20; i++ )  {
+                System.out.print(" " +  i );
+            }
+            System.out.println(" ");
+            int k = 6;
+                while (k >= -6 ) { 
+                    System.out.println(k);
+                        k-=2; }     
+            int x = 10;
+            int sum = 0;
+            do {
+                if (x % 2 == 1) {
+                sum = sum + x;
+            }
+            x++;
+            } while(x < 20);
+            System.out.println("The sum of the odd numbers from 10 to 20: " + sum);
+            }
+        }
--- Lesson_1/Lesson_1/ConditionalStatement.java
+++ Lesson_1/Lesson_1/ConditionalStatement.java
@@ -1,28 +1,31 @@
     public class ConditionalStatement {
         public static void main(String[] args) {
-            int yourAge = 21;
-            if(yourAge > 20) {
+            int Yourage = 21;
+            if(Yourage > 20) {
                 System.out.println("You are young");
             }
-            boolean isMan = true;
-            if(isMan == true) {
+            boolean Areyouaman = true;
+            if(Areyouaman == true) {
                 System.out.println("You're a man");
             }
-            boolean isNotMan = true;
-            if(isNotMan== true) {
+            boolean Areyounotaman = true;
+            if(Areyounotaman== true) {
                 System.out.println("You're not a man");
             }
-            double yourHeight = 1.57;
-            if(yourHeight < 1.80) {
+            double yourheight = 1.57;
+            if(yourheight < 1.80) {
                 System.out.println("Your height is more than 180 centimeters");
             } else {
                 System.out.println("Your height is less than 180 centimeters");
             }
             char DoesyournamebeginwiththeletterM = 'M';
+            char DoesyournamebeginwiththeletterI = 'I';
             if(DoesyournamebeginwiththeletterM == 'M') {
                 System.out.println("Your name begins with the letter M");
+            } else if(DoesyournamebeginwiththeletterI == 'I') {
+                System.out.println("Your name begins with the letter I ");
             } else { 
-                System.out.println("Your name does not begin with the letter M");
+                System.out.println("Your name does not begin with the letter M or I ");
             }
         }
     }
