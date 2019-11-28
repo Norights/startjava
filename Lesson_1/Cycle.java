af53333342d30ff2df40b3364187fab8ea457356
Norights <norights2@gmail.com>
2019-11-28 20:22:25 +0300

    Homework 1 z1 z2 z3 v4

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -7,19 +7,19 @@ public class Cycle {
         System.out.println(" ");
 
         int k = 6;
-        while(k >= -6){ 
+        while(k >= -6) { 
             System.out.println(k);
-            k-=2;
+            k -= 2;
         }
 
         int x = 10;
         int sum = 0;
         do {
-        if(x % 2 == 1) {
-            sum = sum + x;
-        }
-        x++;
+            if(x % 2 == 1) {
+                sum = sum + x;
+            }
+            x++;
         } while(x < 20);
-            System.out.println("The sum of the odd numbers from 10 to 20: " + sum);
+        System.out.println("The sum of the odd numbers from 10 to 20: " + sum);
     }
 }
