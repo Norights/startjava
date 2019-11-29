d95aa4193ecb7f5537adeb630708bf7efac76c7a
Norights <norights2@gmail.com>
2019-11-29 19:27:20 +0300

    Homework 1 z4 z5 z6 v2

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -2,13 +2,16 @@ public class MyFirstGame {
     public static void main(String[] args) {
         int randomNumber = 56;
         int playersNumber = 78;
-        while (playersNumber < randomNumber) 
+        while (playersNumber < randomNumber) {
         playersNumber++;
             System.out.println("Random number is more than you entered"); 
-        while (playersNumber > randomNumber) 
+        }
+        while (playersNumber > randomNumber) {
         playersNumber--;
             System.out.println("Random number is less than you entered");
-        if (playersNumber == randomNumber) 
-        System.out.println("You win");
+        }
+        if (playersNumber == randomNumber) {
+            System.out.println("You win");
+        }
     }
 }
