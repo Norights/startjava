aecf34b0612f1f82827238708e176d398c315c29
Norights <norights2@gmail.com>
2019-12-10 12:52:01 +0300

    Homework 2 z1

---
+++ Lesson_2/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    
+    String gender = "Male";
+    String name = "Nick";
+    int  height = 180;
+    int  weight = 80;
+    int  age = 40;
+    void walk() {
+        System.out.println("He can walk");
+    }
+    boolean sit() {
+        System.out.println("He can sit");
+            return true;
+        }
+    boolean run() {
+        System.out.println("He can run");
+            return true;
+        }
+    String talk() {
+        return "He can talk";
+    }
+    String studyJava() {
+        return "He can study Java";
+    }
+}
