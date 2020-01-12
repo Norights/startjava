f792bc9aba1f3b11ff5220d0079fc88cae8d7efd
Norights <norights2@gmail.com>
2020-01-12 19:24:58 +0300

    Homework 2 z4 V4

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 public class CalculatorTest {
     public static void main(String[] args) {
+        String answer = "no";
+        do {
+        
         Calculator calcOne = new Calculator();
         Scanner scan = new Scanner(System.in);
-
+        
         System.out.println("Enter number 1");
         calcOne.setNum1(scan.nextInt());
-
+        
         System.out.print("Enter an operator (+, -, *, /): ");
-        calcOne.setSign(scan.next().charAt(0)); 
-
+        calcOne.setSign(scan.next().charAt(0));
+        
         System.out.println("Enter number 2");
         calcOne.setNum2(scan.nextInt());
-
-        calcOne.calculate();
-        calcOne.getAns();
-        System.out.println(calcOne.calculate()); 
+      
+        System.out.println(calcOne.calculate());
+        
+        do {
+        System.out.println("Do you want to continue? [yes/no]:" );
+        answer = scan.next();
+        } while (!answer.equals("no") & !answer.equals("yes"));  
+        } while (answer.equals("yes")); 
     }
 }
